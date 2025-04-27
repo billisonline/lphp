91b8634bbb3bc309328f20fc201e5b10d71a6d10
Bill Yanelli <bill.yanelli@gmail.com>
2025-04-27 17:18:25 -0400

    Improve script and build files isolation

--- build.sh
+++ build.sh
@@ -1,11 +1,26 @@
+#!/bin/bash
+
 set -e
 
-sudo rm -rf buildroot downloads source 
-
+VERSION="8.4"
 EXTENSIONS="apcu,bcmath,calendar,ctype,curl,dba,dom,exif,fileinfo,filter,gd,iconv,intl,mbregex,mbstring,mysqli,mysqlnd,opcache,openssl,pcntl,pdo,pdo_mysql,pdo_pgsql,pdo_sqlite,pgsql,phar,posix,readline,redis,session,simplexml,sockets,sodium,sqlite3,tokenizer,xml,xmlreader,xmlwriter,xsl,zip,zlib"
+SPC_URL="https://github.com/crazywhalecc/static-php-cli/releases/download/2.5.2/spc-macos-aarch64.tar.gz"
 
-./spc download --with-php=8.4 --for-extensions "$EXTENSIONS" --prefer-pre-built
+# Special cmake cask to install 3.31.6, the latest version to be supported by spc
+brew install -s ./cmake.rb
+
+mkdir -p build/output
+
+cd build
+
+curl -Lo spc.tar.gz "$SPC_URL"
+
+tar -xvzf spc.tar.gz
+
+./spc download "--with-php=$VERSION" --for-extensions "$EXTENSIONS" --prefer-pre-built
 
 ./spc spc-config --with-suggested-libs --with-suggested-exts "$EXTENSIONS"
 
 ./spc build --debug --build-cli --with-suggested-libs --with-suggested-exts "$EXTENSIONS"
+
+cp ./build/buildroot/bin/php ./build/output/php
