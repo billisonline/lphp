20e1c52a572900dfdcb6dd27948124883c9aa319
Bill Yanelli <bill.yanelli@gmail.com>
2025-05-06 17:41:29 -0400

    Change name to phpw

--- ci/generate-installer-patch.sh
+++ ci/generate-installer-patch.sh
@@ -1,8 +1,8 @@
-TEMPLATE_FILE="templates/vphp"
+TEMPLATE_FILE="templates/phpw"
 
-echo "diff --git a/vphp b/vphp"
+echo "diff --git a/phpw b/phpw"
 echo "new file mode 100755"
 echo "--- /dev/null"
-echo "+++ ./vphp"
+echo "+++ ./phpw"
 echo "@@ -0,0 +1,$(cat "$TEMPLATE_FILE" | wc -l | xargs) @@"
 cat "$TEMPLATE_FILE" | while IFS= read -r line; do echo "+${line}"; done
